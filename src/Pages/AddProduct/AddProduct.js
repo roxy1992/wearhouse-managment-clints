64436fb22f8ea7e1c8bb053be168d4ee79186d25
roxy1992 <roxybhuiyan1992@gmail.com>
2022-05-07 15:38:48 +0600

    add delete option

--- src/Pages/AddProduct/AddProduct.js
+++ src/Pages/AddProduct/AddProduct.js
@@ -22,7 +22,7 @@ const AddProduct = () => {
 
     return (
         <div className='w-50 mx-auto'>
-            <h2>pleass add a product</h2>
+            <h2 className='text-center'>pleass add a product</h2>
             <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
                 <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
                 <textarea className='mb-2' placeholder='Description' {...register("description")} />
--- src/Pages/ManageProducts/ManageProducts.js
+++ src/Pages/ManageProducts/ManageProducts.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useProducts from '../../hooks/useProducts';
 
 const ManageProducts = () => {
-    const [products,] = useProducts();
+    const [products, setProducts] = useProducts();
 
     const handleDelete = id => {
         const proceed = window.confirm('Are you sure?');
         if (proceed) {
-
+            const url = `http://localhost:5000/service/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = products.filter(product => product._id !== id)
+                    setProducts(remaining)
+                })
         }
     }
     return (
         <div className='w-50 mx-auto'>
-            <h2>Manage your services</h2>
+            <h2 className='text-center'>Manage Your Product</h2>
             {
                 products.map(product => <div key={products.id}>
                     <h5>{product.name} <button onClick={() => handleDelete(product._id)}>x</button></h5>
